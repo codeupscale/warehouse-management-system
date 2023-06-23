141cc6ca471c0abe08cbc62db1b459c80acbe229
Abdur Rehman <abdur.rehman@codeupscale.com>
2023-06-23 17:10:31 +0500

    WMS-19 | prefix added in routes

--- routes/web.php
+++ routes/web.php
@@ -35,12 +35,16 @@ Route::middleware(['auth','user-access:admin'])->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::resource('customers',CustomerController::class);
-    Route::resource('warehouses', WarehouseController::class);
-    Route::post('users/{id}/update',[UserController::class,'update'])->name('users.update');
-    Route::resource('users',UserController::class)->except('update');
-    Route::resource('stocks',StockController::class);
-    Route::resource('stockItems',StockItemController::class);
+    Route::prefix('api')->group(function () {
+        Route::resource('customers',CustomerController::class);
+        Route::resource('warehouses', WarehouseController::class);
+        Route::post('users/{id}/update',[UserController::class,'update'])->name('users.update');
+        Route::resource('users',UserController::class)->except('update');
+        Route::resource('stocks',StockController::class);
+        Route::resource('stockItems',StockItemController::class);
+
+    });
+
     Route::get('/warehouses/{id}/stocks', [WarehouseController::class,'getAllStocks'])->name('warehouses.stock');
     Route::get('/warehouses/stock/{id}/stockItems', [StockController::class,'getStockItems'])->name('stock.stockItems');    
 
